7e8499464af23f179c52271e4223ec00ea13be51
Atharv Salian <atharv44@gmail.com>
2022-01-26 18:31:32 +0530

    javascript basic

--- practice/src/index.js
+++ practice/src/index.js
@@ -1,7 +1,16 @@
 document.addEventListener('DOMContentLoaded', () =>{
 
-    
-
+let count = 0
+function increment(){
+    count = count+1
+    console.log(count)
+}
+increment()
+const name = 'Atharv'
+function sendText(){
+    console.log('hey '+name+' you left your bag')
+}
+sendText()
 
 
 })//Or you can write the script tag below all the html code in the body
