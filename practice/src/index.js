7f515b5e9c269310700a2b76eb488e399d1e4e6d
Atharv56 <69430707+Atharv56@users.noreply.github.com>
2022-11-08 17:49:17 +0530

    images

--- practice/src/index.js
+++ practice/src/index.js
@@ -1,15 +1,26 @@
-document.write(20+11)
-window.alert("Welcome my friend")
+// document.getElementById('demo').innerHTML = 5+6
+// document.getElementById('demo').style.fontSize = "35px"
+// document.getElementById('demo').style.display = "block"
+// document.write(5+7)
+// window.alert("Welcome to my practice javascript session")
+// console.log("welcome to my console")
+// console.log(5+10)
+// let x, y, z;
+// x = 5;
+// y = 6;
 
-let a , b, c
-a = 5 
-b = 6
-c = 10
+// z = x+y;
+// console.log("Hello " + z + '.')
 
-console.log(a+b+c)
-function myFunction(){
-   document.getElementById("demo").innerHTML = "Hello Atharv"
-
+// function myFunction(){
+//    console.log("Hello this is my first javascript function.");
+//    console.log(10*2);
+   
+// }
+// console.log((x == y))
+// myFunction();
+function multiplication(n1 , n2){
+   return n1*n2
 }
-myFunction()
-console.log(a!==b)
+let x = multiplication(10, 2)
+console.log(x)
